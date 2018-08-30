5d02bdd5f51f42cb4a931e5d7b04c36f7f85970a
drubaby <peryton6@gmail.com>
2018-08-30 14:01:48 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+    # song.artist = self
+  end
+
+  def self.song_count
+    Song.all.length
+  end
+  # binding.pry
+# Artist.song_count = 2
+
+end
